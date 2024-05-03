0ecf778b0fac7e8b62b2791c3d56adeeb904bb6c
wangwei83 <wangwei83@cuit.edu.cn>
2024-05-03 21:25:12 +0800

    16line_filtered 滤波代码并提交

---
+++ 16line_filtered.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <pcl/io/pcd_io.h>
+#include <pcl/point_types.h>
+#include <pcl/filters/passthrough.h>
+
+int main(int argc, char** argv)
+{
+	pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>);
+	pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_filtered(new pcl::PointCloud<pcl::PointXYZ>);
+
+	// Fill in the cloud data
+	pcl::PCDReader reader;
+	reader.read("16line.pcd", *cloud);
+	std::cerr << "Cloud before filtering: " << cloud->points.size() << std::endl;
+	// Create the filtering object
+	pcl::PassThrough<pcl::PointXYZ> pass; // 声明直通滤波
+	pass.setInputCloud(cloud); // 传入点云数据
+	pass.setFilterFieldName("z"); // 设置操作的坐标轴
+	pass.setFilterLimits(0.0, 3.0); // 设置坐标范围
+	// pass.setFilterLimitsNegative(true); // 保留数据函数
+	pass.filter(*cloud_filtered);  // 进行滤波输出
+
+	std::cerr << "Cloud after filtering: " << cloud_filtered->points.size() << std::endl;
+
+	// save filterd data 
+	pcl::PCDWriter writer;
+	writer.write("16line_filtered.pcd", *cloud_filtered, false);
+
+	return 0;
+}
